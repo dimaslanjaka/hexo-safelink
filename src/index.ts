9bc7e80fc681d0f3e320cf925428f77b8889d90f
Dimas Lanjaka <dimaslanjaka@gmail.com>
2023-08-27 17:26:51 +0000

    initialize options

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
 import Hexo from 'hexo';
+import * as safelink from 'safelinkify';
+
+const defaultOptions = {
+  // exclude patterns (dont anonymize these patterns)
+  exclude: [
+    'domain.com',
+    /another.domain.com/,
+    /https?:\/\/?(?:([^*]+)\.)?webmanajemen\.com/,
+    /([a-z0-9](?:[a-z0-9-]{1,61}[a-z0-9])?[.])*webmanajemen\.com/
+  ],
+  // url redirector
+  redirect: 'https://www.webmanajemen.com/page/safelink.html?url=',
+  // debug
+  verbose: false,
+  // encryption type = 'base64' | 'aes'
+  type: 'base64',
+  // password aes, default = root
+  password: 'unique-password'
+};
 
 export interface HexoSafelinkConfig {
-  //
+  exclude: (string | RegExp)[];
+  redirect: string;
+  verbose: boolean;
+  type: string;
+  password: string;
 }
 
-export default function HexoSafelink(hexo: Hexo) {
-  const config: HexoSafelinkConfig = hexo.config.safelink;
+export function HexoSafelink(hexo: Hexo) {
+  const options: HexoSafelinkConfig = hexo.config.safelink;
+  const sf = new safelink.safelink(options);
 }
