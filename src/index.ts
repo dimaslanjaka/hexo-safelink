4649d6371df968a6092ab113e2f134c21647fe6d
Dimas Lanjaka <dimaslanjaka@gmail.com>
2023-08-27 17:07:04 +0000

    moved into lib

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import Hexo from 'hexo';
-
-export interface HexoSafelinkConfig {
-  //
-}
-
-export default function HexoSafelink(hexo: Hexo) {
-  const config: HexoSafelinkConfig = hexo.config.safelink;
-}
