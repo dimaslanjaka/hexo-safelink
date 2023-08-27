a390c55bf74df83efd45155d3c8ad1013831f816
Dimas Lanjaka <dimaslanjaka@gmail.com>
2023-08-27 17:04:41 +0000

    initialize config

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export default function HexoSafelink(hexo) {
+import Hexo from 'hexo';
+
+export interface HexoSafelinkConfig {
   //
+}
+
+export default function HexoSafelink(hexo: Hexo) {
+  const config: HexoSafelinkConfig = hexo.config.safelink;
 }
